e8ca1a905649e3035927c9acd3497934f0b35a7b
Bernadette Aquino <bernadette.p.aquino@gmail.com>
2026-01-27 07:47:37 +0800

    completed bubble sort

--- sorting_iterative.py
+++ sorting_iterative.py
@@ -11,10 +11,31 @@ def is_sorted(items):
 def bubble_sort(items):
     """Sort given items by swapping adjacent items that are out of order, and
     repeating until all items are in sorted order.
-    TODO: Running time: ??? Why and under what conditions?
-    TODO: Memory usage: ??? Why and under what conditions?"""
-    # TODO: Repeat until all items are in sorted order
-    # TODO: Swap adjacent items that are out of order
+    Running Time: 
+        Worst case (ex. reversed array): O(n^2): quadratic time, as the array gets bigger, the time grows really fast
+        Best case (ex. sorted array): O(n): linear time
+        Average case (ex. random array): O(n^2), quadratic time, same as worst case
+    Memory usage: ??? Why and under what conditions
+        O(1): sorts in-place since we're not growing the array size
+    """
+        
+    # Original Array: [10, 7, 1, 3, 6]
+    # [7, 10, 1, 3, 6] --> first 0, 1
+    # [7, 1, 10, 3, 6] --> index 1, 2
+    # Repeat this process until the array is sorted:
+    #   Look into the first pair of the array (first and second item on the list)
+    #   Compare them using operator > or <
+    #   Then move to the second pair (second and third item on the list)
+    #   Do this until the items array is sorted
+    swapped = True    
+    while swapped:
+        swapped = False
+        for index in range(len(items) - 1):
+            if items[index] > items[index + 1]:
+                original_value = items[index]
+                items[index] = items[index + 1]
+                items[index + 1] = original_value
+                swapped = True
 
 
 def selection_sort(items):
